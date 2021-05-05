9f622678b9e522ef848ef838b23f6256e0d3586f
Victor <maricatovictor@gmail.com>
2021-05-04 22:41:01 -0300

    simple flask front + fastapi back

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/query")
+def query(q: str) -> str:
+    return q
