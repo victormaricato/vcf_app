b40e9334f0d934bfa34222cb427e862ee89f2e8c
Victor <maricatovictor@gmail.com>
2021-05-09 15:55:05 -0300

    Refactoring

--- backend/database/repository.py
+++ backend/database/repository.py
@@ -3,10 +3,9 @@ from sqlalchemy.orm import Session
 from .models import Variant
 
 
-def query_by_chr_and_pos(db: Session, chr: str, pos: str):
+def query_by_chr_and_pos(db: Session, chr: str, pos: str) -> Variant:
     return db.query(Variant).filter((Variant.chromosome == chr) & (Variant.position == pos)).first()
 
 
-def query_by_rsid(db: Session, rsid: str):
-    print(rsid)
+def query_by_rsid(db: Session, rsid: str) -> Variant:
     return db.query(Variant).filter(Variant.rsid == rsid).first()
